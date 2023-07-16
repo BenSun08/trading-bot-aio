d45432dbeb6d934693339d82d2ca57da1ab81d3c
SanWang <bensun08@outlook.com>
2023-07-16 17:58:06 +0800

    git subrepo clone https://github.com/KevynUtopia/RL-trading-bot.git trading_bot/ext
    subrepo:
      subdir:   "trading_bot/ext"
      merged:   "f1993b7"
    upstream:
      origin:   "https://github.com/KevynUtopia/RL-trading-bot.git"
      branch:   "main"
      commit:   "f1993b7"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

---
+++ trading_bot/ext/README.md
@@ -0,0 +1,3 @@
+[bitcoin dataset](https://www.kaggle.com/datasets/prasoonkottarathil/btcinusd) to ./dataset/archive/
+
+[source code reference](https://github.com/pskrunner14/trading-bot)
---
+++ trading_bot/ext/train.py
@@ -0,0 +1,103 @@
+"""
+Script for training Stock Trading Bot.
+
+Usage:
+  train.py <train-stock> <val-stock> [--strategy=<strategy>]
+    [--window-size=<window-size>] [--batch-size=<batch-size>]
+    [--episode-count=<episode-count>] [--model-name=<model-name>]
+    [--pretrained] [--debug] [--sample-rate=<sample-rate>] [--balance=<balance>]
+
+Options:
+  --strategy=<strategy>             Q-learning strategy to use for training the network. Options:
+                                      `dqn` i.e. Vanilla DQN,
+                                      `t-dqn` i.e. DQN with fixed target distribution,
+                                      `double-dqn` i.e. DQN with separate network for value estimation. [default: t-dqn]
+  --window-size=<window-size>       Size of the n-day window stock data representation
+                                    used as the feature vector. [default: 10]
+  --batch-size=<batch-size>         Number of samples to train on in one mini-batch
+                                    during training. [default: 32]
+  --episode-count=<episode-count>   Number of trading episodes to use for training. [default: 50]
+  --model-name=<model-name>         Name of the pretrained model to use. [default: model_debug]
+  --pretrained                      Specifies whether to continue training a previously
+                                    trained model (reads `model-name`).
+  --debug                           Specifies whether to use verbose logs during eval operation.
+"""
+
+import logging
+import coloredlogs
+
+from docopt import docopt
+import pandas as pd
+import random
+
+
+from trading_bot.agent import Agent
+from trading_bot.methods import train_model, evaluate_model
+from trading_bot.utils import (
+    get_stock_data,
+    format_currency,
+    format_position,
+    show_train_result,
+    switch_k_backend_device
+)
+
+
+def main(train_stock, val_stock, window_size, batch_size, ep_count,
+         strategy="t-dqn", model_name="model_debug", pretrained=False,
+         debug=False, sample_rate=0.3, balance=1000.0):
+    """ Trains the stock trading bot using Deep Q-Learning.
+    Please see https://arxiv.org/abs/1312.5602 for more details.
+
+    Args: [python train.py --help]
+    """
+    agent = Agent(window_size, strategy=strategy, pretrained=pretrained, model_name=model_name, balance=balance)
+    
+    # df = pd.read_csv(train_stock)
+    # train_data = get_stock_data(train_stock)[:int(len(df)*0.1)]
+    # df = pd.read_csv(val_stock)
+    # val_data = get_stock_data(val_stock)[:int(len(df)*0.1)]
+
+    df = pd.read_csv(train_stock)
+
+    train_data = get_stock_data(train_stock)#[:int(len(df))]
+    # df = pd.read_csv(val_stock)
+    part = random.random()*0.9
+    # val_data = get_stock_data(train_stock)[int(len(df)*part):int(len(df)*(part+0.02))]
+    if 'BTC' in  train_stock:
+      val_data = get_stock_data(train_stock)[int(len(df)*0.3):int(len(df)*0.32)]
+    else:
+      val_data = get_stock_data(val_stock)
+
+    initial_offset = val_data[1] - val_data[0]
+
+    for episode in range(1, ep_count + 1):
+        train_result = train_model(agent, episode, train_data, ep_count=ep_count,
+                                   batch_size=batch_size, window_size=window_size, sample_rate=sample_rate)
+        val_result, _ = evaluate_model(agent, val_data, window_size, debug)
+        show_train_result(train_result, val_result, initial_offset)
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+
+    train_stock = args["<train-stock>"]
+    val_stock = args["<val-stock>"]
+    strategy = args["--strategy"]
+    window_size = int(args["--window-size"])
+    batch_size = int(args["--batch-size"])
+    ep_count = int(args["--episode-count"])
+    model_name = args["--model-name"]
+    pretrained = args["--pretrained"]
+    debug = args["--debug"]
+    sample_rate = float(args["--sample-rate"])
+    balance = float(args["--balance"])
+
+    coloredlogs.install(level="DEBUG")
+    switch_k_backend_device()
+
+    try:
+        main(train_stock, val_stock, window_size, batch_size,
+             ep_count, strategy=strategy, model_name=model_name, 
+             pretrained=pretrained, debug=debug, sample_rate=sample_rate, balance=balance)
+    except KeyboardInterrupt:
+        print("Aborted!")
